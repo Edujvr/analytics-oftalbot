a90827bd75035feed45cf8fcf77ba961a3311c3c
Edujvr <32462670+Edujvr@users.noreply.github.com>
2017-11-13 14:53:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ app.use(require('body-parser').json());
 const router = express.Router();
 
 
-router.post('/webhook/ai', (req, res, next) => {  
+router.post('/webhook', (req, res, next) => {  
   const action = req.body.results.action; // 'track_event'
   switch(action) {   
     case 'track_event':
-        const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid<UA-109367761-1>&cid=${request.body.sessionId}&dh<https://www.google-analytics.com>&ec=Intent&ea=${request.body.result.metadata.intentName}&ev=1&aip=1';
+        const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid<UA-109367761-1>&cid=${request.body.sessionId}&dh<www.google-analytics.com>&ec=Intent&ea=${request.body.result.metadata.intentName}&ev=1&aip=1';
         require('request').get(url)
         .then(err => {
           if (err) throw err;
