613d0bbcc757fedefd92a699b0eeb3bc41ddf1e3
Edujvr <32462670+Edujvr@users.noreply.github.com>
2017-11-13 14:56:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ router.post('/webhook', (req, res, next) => {
   switch(action) {   
     case 'track_event':
         const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid<UA-109367761-1>&cid=${request.body.sessionId}&dh<www.google-analytics.com>&ec=Intent&ea=${request.body.result.metadata.intentName}&ev=1&aip=1';
-        require('request')..get(encodeURI(url))
+        require('request').get(encodeURI(url))
         .then(err => {
           if (err) throw err;
           console.log('Successfully logged to GA');
