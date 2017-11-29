dede5c420a7489799c5c9f39ade5a8a2ac355673
Edujvr <32462670+Edujvr@users.noreply.github.com>
2017-11-28 19:53:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,30 @@
 const express = require('express');
 const app = express();
 app.use(require('body-parser').json());
-
+app.listen(process.env.PORT || 8080);
+console.log('Inicio el programa');
 const router = express.Router();
 
-router.post('/web', (req, res, next) => {  
+app.post("/webhook", (req, res, next) => {  
+console.log('entro aqui');
   const action = req.body.result.action;
+  console.log(action);
   switch(action) {   
-    case 'track_event':
+    case 'prueba':
         const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-109367761-1&cid=${request.body.sessionId}&dh=www.google-analytics.com&ec=Intent&ea=${request.body.result.metadata.intentName}&ev=1&aip=1';
-        require('request').get(encodeURI(url))
-        .then(err => {
+        var request = require('request');
+		request.get(encodeURI(url))
+        .on('error', function(err){
           if (err) throw err;
-          console.log('Successfully logged to GA');
+          else{
+			  console.log('Successfully logged to GA');
           res.json({
             messages: req.body.result.fulfillment.messages,
             speech: req.body.result.fulfillment.speech,
             displayText: req.body.result.fulfillment.speech,
             contextOut: req.body.result.contexts,
             source: req.body.result.source
+		  }
           });
         });
       break; 
