6d8879b78f55761c2c5325b8b610c678ed5e0c90
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-01-19 15:05:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post("/webhook", (req, res, next) => {
       break; 
 	case 'nothandled':
 //Envio de información a Chatbase libreria @google/chatbase
-	var msg = chatbase.newMessage('c0f0424f-cf81-4f54-8287-006327e7bf4d', req.body.sessionId)
+	/*var msg = chatbase.newMessage('c0f0424f-cf81-4f54-8287-006327e7bf4d', req.body.sessionId)
 	.setPlatform('Dialogflow') 
 	.setMessage(req.body.result.resolvedQuery) 
 	.setIntent(req.body.result.metadata.intentName)  
@@ -48,7 +48,7 @@ app.post("/webhook", (req, res, next) => {
 	.setMessageId(req.body.id) 
 	.send()
 	.then(msg => console.log(msg.getCreateResponse()))
-	.catch(err => console.error(err));
+	.catch(err => console.error(err));*/
 //Envio de información a Google Analytics libreria request
 	const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-109367761-1&cid='+req.body.sessionId+'&dh=www.google-analytics.com&ec=Intento&ea='+req.body.result.metadata.intentName+'&el='+req.body.result.resolvedQuery+'&ev=1&aip=1';
      	var request = require('request');
@@ -58,13 +58,13 @@ app.post("/webhook", (req, res, next) => {
 	  	console.log('Successfully logged to GA , Response to Dialogflow');
         });
 //Envio de información webhook a Dialogflow		  
-	/*res.json({
+	res.json({
             messages: req.body.result.fulfillment.messages,
             speech: req.body.result.fulfillment.speech,
             displayText: req.body.result.fulfillment.speech,
             contextOut: req.body.result.contexts,
             source: req.body.result.source
-          });*/ 
+          }); 
       break; 
   }
 });
