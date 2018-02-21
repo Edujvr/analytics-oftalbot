6a56d126801242b112c258643d9a9d1c572f1627
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-02-20 22:13:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 app.use(require('body-parser').json());
 app.listen(process.env.PORT || 8080);
-//var chatbase = require('@google/chatbase');
+var chatbase = require('@google/chatbase');
 var request = require('request');
 
 app.post("/webhook", (req, res, next) => {  
@@ -20,13 +20,10 @@ app.post("/webhook", (req, res, next) => {
 	
 switch(action) {   
     case 'control':
- 	 var chatbase = require('@google/chatbase')
-	 .setAsTypeAgent(bot)
-	 .setMessage(req.body.result.fulfillment.speech)
-	 .setPlatform('Dialogflow');
+ 	  
 //Envio de información a Chatbase libreria @google/chatbase
 	var msgUser = chatbase.newMessage('c0f0424f-cf81-4f54-8287-006327e7bf4d', req.body.sessionId)
-	.setAsTypeUser(user)
+	.setAsTypeAgent()
 	.setPlatform('Dialogflow') 
 	.setMessage(req.body.result.resolvedQuery) 
 	.setIntent(req.body.result.metadata.intentName)  
@@ -36,13 +33,13 @@ switch(action) {
 	.then(msgUser => console.log(msgUser.getCreateResponse()))
 	.catch(err => console.error(err));
 		
-	/*var msgAgent = chatbase.newMessage('c0f0424f-cf81-4f54-8287-006327e7bf4d', req.body.sessionId)
+	var msgAgent = chatbase.newMessage('c0f0424f-cf81-4f54-8287-006327e7bf4d', req.body.sessionId)
 	.setAsTypeUser()
 	.setIntent(req.body.result.metadata.intentName)
 	.setMessage(req.body.result.fulfillment.speech)
 	.send()
 	.then(msgAgent => console.log(msgAgent.getCreateResponse()))
-	.catch(err => console.error(err));*/
+	.catch(err => console.error(err));
 		
 //Envio de información a Google Analytics libreria request
 	const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-109367761-1&cid='+req.body.sessionId+'&dh=www.google-analytics.com&ec=Intento&ea='+req.body.result.metadata.intentName+'&el='+req.body.result.resolvedQuery+'&ev=1&aip=1';
@@ -53,10 +50,9 @@ switch(action) {
         });
       break; 
 	case 'nothandled':
-	var chatbase = require('@google/chatbase')
 //Envio de información a Chatbase libreria @google/chatbase
 	var msgUser = chatbase.newMessage('c0f0424f-cf81-4f54-8287-006327e7bf4d', req.body.sessionId)
-	.setAsTypeUser(user)
+	.setAsTypeUser('user')
 	.setPlatform('Dialogflow') 
 	.setMessage(req.body.result.resolvedQuery) 
 	.setIntent(req.body.result.metadata.intentName)  
@@ -68,7 +64,7 @@ switch(action) {
 	.catch(err => console.error(err));
 		
 	var msgAgent = chatbase.newMessage('c0f0424f-cf81-4f54-8287-006327e7bf4d', req.body.sessionId)
-	.setAsTypeAgent(bot)
+	.setAsTypeAgent('agent')
 	.setIntent(req.body.result.metadata.intentName)
 	.setMessage(req.body.result.fulfillment.speech)
 	.send()
