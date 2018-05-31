7a699a768c51fee46a985518d8a505deec0d8d42
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-05-31 11:30:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const express = require('express');
 const app = express();
-app.use(require('body-parser').json());
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.listen(process.env.PORT || 8080);
 var request = require('request');
 const taskController = require("./controllers/TaskController");
