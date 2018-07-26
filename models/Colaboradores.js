07926af6731e5aa86ad48d8e913ac5a2e9fcc873
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-07-26 14:50:00 -0500

    Update Colaboradores.js

--- models/Colaboradores.js
+++ models/Colaboradores.js
@@ -1,1 +1,15 @@
-a
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ColaboradoresSchema = new Schema({
+  userid:{
+        type: String,
+        required:true
+  },
+  consultor: {
+    type: String,
+    required:true
+  }
+});
+
+module.exports = mongoose.model("Colaboradores", ColaboradoresSchema);
