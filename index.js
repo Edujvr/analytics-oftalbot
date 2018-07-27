ae6c433941493bcd23012dc22085a3e80dbf3de7
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-07-26 22:27:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post("/webhook", (req, res) =>{
 		colaborador.userid = req.body.result.parameters.UsuariosRed;
 		console.log('Ingreso al metodo de consulta');
 		console.log(req.body.result.parameters.UsuariosRed);
-		var colaborador=Colaboradores.find(colaborador, (err, colaboradores) => {
+		var cola.consultor =Colaboradores.find(colaborador, (err, colaboradores) => {
 		    if (err) {
 		      res.status(500).send(err);
 		    }
