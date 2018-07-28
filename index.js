37eb7b91898c71fbd6e365b529f7e977487cf6d4
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-07-28 00:09:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -40,16 +40,32 @@ app.post("/webhook", (req, res) =>{
 		    }
 			var respuesta = "Tu consultor es :" + colaboradores.consultor 
 			console.log(respuesta)
-			res.json({
-			    messages: req.body.result.fulfillment.messages,
-			    speech: req.body.result.fulfillment.speech,
-			    displayText: req.body.result.fulfillment.speech,
-			    contextOut: req.body.result.contexts,
-			    source: req.body.result.source
-			  });
+			sendResponse(respuesta);
 		  });
 		//console.log("Tu consultor es : " + cola);
 	}
+function sendResponse (responseToUser) {
+    // if the response is a string send it as a response to the user
+    if (typeof responseToUser === 'string') {
+      let responseJson = {};
+      responseJson.speech = responseToUser; // spoken response
+      responseJson.displayText = responseToUser; // displayed response
+      response.json(responseJson); // Send response to Dialogflow
+    } else {
+      // If the response to the user includes rich responses or contexts send them to Dialogflow
+      let responseJson = {};
+      // If speech or displayText is defined, use it to respond (if one isn't defined use the other's value)
+      responseJson.speech = responseToUser.speech || responseToUser.displayText;
+      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
+      // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
+      responseJson.data = responseToUser.data;
+      // Optional: add contexts (https://dialogflow.com/docs/contexts)
+      responseJson.contextOut = responseToUser.outputContexts;
+      console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
+      response.json(responseJson); // Send response to Dialogflow
+    }
+  }
+}
 	
   //Envio de información webhook a Dialogflow		  
 /*	res.json({
