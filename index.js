9346fac1bbf0f0c5f536be539c94b5b709edc046
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-08-02 17:53:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ app.post("/webhook", (req, res) =>{
 	if(action=='query'){
 		var query  = Colaboradores.where({ UsuarioRed: req.body.result.parameters.UsuariosRed });
 		//const usuarioName= req.body.originalRequest.data.user.user_id;
-		console.log(req);
+		var obj = JSON.parse(req);
+		console.log(obj);
 		console.log('Ingreso al metodo de consulta');
 		console.log(req.body.result.parameters.UsuariosRed);
 		query.findOne(function (err, colaboradores) {
