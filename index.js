0b5d0ca033f5f242c034493d0c79a3d1cbd205f1
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-08-13 10:19:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,69 +14,25 @@ require("./config/db");
 
 //Creación del metodo que escucha las llamadas POST y obtiene los parametros
 app.post("/webhook", (req, res) =>{  
-	
-/*let newTask = new Task(req.body);
-  newTask.save((err, task) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(201).json(task);
-  });*/
-  console.log(req.body.originalRequest)
-	
-	
+  //console.log(req.body.originalRequest)	
   const Colaboradores = require("./models/Colaboradores");
   const action = req.body.result.action;
   const chatbase = require('@google/chatbase');
   const chatbase2= require('@google/chatbase');	
-  
-  console.log(action);	
-	if(action=='query'){
-		var query  = Colaboradores.where({ UsuarioRed: req.body.result.parameters.UsuariosRed });
-		query.findOne(function (err, colaboradores) {
-		    if (err) {
-		      res.status(500).send(err);
-		    }
-			var respuesta =colaboradores.Nombre +" Tu consultor es " + colaboradores.NombreConsultor //+" Tu nombre " +usuarioName
-			sendResponse(respuesta);
-		  });
-	}
 	
-function sendResponse (responseToUser) {
-    // Si la respuesta es una cadena, envíela como respuesta al usuario
-    if (typeof responseToUser === 'string') {
-      let responseJson = {};
-      responseJson.speech = responseToUser; // respuesta hablada
-      responseJson.displayText = responseToUser; // respuesta mostrada
-      res.json(responseJson); // Enviar respuesta a Dialogflow
-    } else {
-      // Si la respuesta al usuario incluye respuestas ricas o contextos, envíelos a Dialogflow
-      let responseJson = {};
-      // Si speech o displayText está definido, úselo para responder (si uno no está definido use el valor del otro)
-      responseJson.speech = responseToUser.speech || responseToUser.displayText;
-      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
-      // Opcional: agregue mensajes enriquecidos para integraciones (https://dialogflow.com/docs/rich-messages)
-      responseJson.data = responseToUser.data;
-      // Opcional: agregar contextos (https://dialogflow.com/docs/contexts)
-      responseJson.contextOut = responseToUser.outputContexts;
-      console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
-      res.json(responseJson); // Enviar respuesta a Dialogflow
-    }
-  }
-
-	
-  //Envio de información webhook a Dialogflow		  
-	res.json({
-            messages: req.body.result.fulfillment.messages,
-            speech: req.body.result.fulfillment.speech,
-            displayText: req.body.result.fulfillment.speech,
-            contextOut: req.body.result.contexts,
-            source: req.body.result.source
-          });
-/*	
+	/*
+	//Envio de Mensaje a Mongo Atlas
+	let newTask = new Task(req.body);
+	  newTask.save((err, task) => {
+	    if (err) {
+	      res.status(500).send(err);
+	    }
+	    res.status(201).json(task);
+	  });*/
+  	
 	// Creación mensaje Set de Usuario
 	var messageSet = chatbase.newMessageSet()
-	  .setApiKey("c0f0424f-cf81-4f54-8287-006327e7bf4d") // Chatbase API key
+	  .setApiKey("f8be6699-d8b4-44d8-90cb-07d8d2e98cf2") // Chatbase API key
 	  .setPlatform("Dialogflow") // Nombre de la Plataforma del Chat
 	  .setVersion('1.0'); // La versión que el bot desplegado es
 
@@ -110,7 +66,7 @@ function sendResponse (responseToUser) {
 	
 	// Creación mensaje Set del Bot
 	var messageSet2 = chatbase.newMessageSet()
-	  .setApiKey("c0f0424f-cf81-4f54-8287-006327e7bf4d") // Chatbase API key
+	  .setApiKey("f8be6699-d8b4-44d8-90cb-07d8d2e98cf2") // Chatbase API key
 	  .setPlatform("Dialogflow") // Nombre de la Plataforma del Chat
 	  .setVersion('1.0'); // La versión que el bot desplegado es
 	
@@ -131,10 +87,60 @@ function sendResponse (responseToUser) {
 	});
 		
 //Envio de información a Google Analytics libreria request
-	const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-109367761-1&cid='+req.body.sessionId+'&dh=www.google-analytics.com&ec=Intento&ea='+req.body.result.metadata.intentName+'&el='+req.body.result.resolvedQuery+'&ev=1&aip=1';
+	const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-123508749-1&cid='+req.body.sessionId+'&dh=www.google-analytics.com&ec=Intento&ea='+req.body.result.metadata.intentName+'&el='+req.body.result.resolvedQuery+'&ev=1&aip=1';
 		request.get(encodeURI(url))
        		.on('error', function(err){
           	if (err) throw err;
 	  	console.log('Successfully logged to GA , Response to Dialogflow');
-        });*/
-});
+        });	
+	
+	if(action=='query'){
+		var query  = Colaboradores.where({ UsuarioRed: req.body.result.parameters.UsuariosRed });
+		query.findOne(function (err, colaboradores) {
+		    if (err) {
+		      res.status(500).send(err);
+		    }
+			var respuesta =colaboradores.Nombre +" Tu consultor es " + colaboradores.NombreConsultor //+" Tu nombre " +usuarioName
+			sendResponse(respuesta);
+		  });
+	 } else { //Envio de información webhook a Dialogflow		  
+	    res.json({
+		    messages: req.body.result.fulfillment.messages,
+		    speech: req.body.result.fulfillment.speech,
+		    displayText: req.body.result.fulfillment.speech,
+		    contextOut: req.body.result.contexts,
+		    source: req.body.result.source
+       		 });
+	 }
+	
+	//Envio de información webhook a Dialogflow Messenger
+         function sendResponse (responseToUser) {
+	    // Si la respuesta es una cadena, envíela como respuesta al usuario
+	    if (typeof responseToUser === 'string') {
+	      let responseJson = {};
+	      responseJson.speech = responseToUser; // respuesta hablada
+	      responseJson.displayText = responseToUser; // respuesta mostrada
+	      res.json(responseJson); // Enviar respuesta a Dialogflow
+	    } else {
+	      // Si la respuesta al usuario incluye respuestas ricas o contextos, envíelos a Dialogflow
+	      let responseJson = {};
+	      // Si speech o displayText está definido, úselo para responder (si uno no está definido use el valor del otro)
+	      responseJson.speech = responseToUser.speech || responseToUser.displayText;
+	      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
+	      // Opcional: agregue mensajes enriquecidos para integraciones (https://dialogflow.com/docs/rich-messages)
+	      responseJson.data = responseToUser.data;
+	      // Opcional: agregar contextos (https://dialogflow.com/docs/contexts)
+	      responseJson.contextOut = responseToUser.outputContexts;
+	      console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
+	      res.json(responseJson); // Enviar respuesta a Dialogflow
+	    }
+	  }
+	
+	
+	
+    });
+	
+
+
+	
+
