9ae1dca680e559d28b218874d8d36ca1f9014898
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-08-13 14:26:16 -0500

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const HistorialSchema = new Schema({
+const TaskSchema = new Schema({
   result:{
     resolvedQuery:{
       type: String,
