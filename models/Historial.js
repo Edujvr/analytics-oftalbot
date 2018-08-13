aec80cd7dddb20760446c8b242e005bf1e82c1d1
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-08-13 11:29:33 -0500

    Create Historial.js

---
+++ models/Historial.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+  result:{
+    resolvedQuery:{
+      type: String,
+      required:true,
+    },
+    metadata:{
+      intentName:{
+        type: String,
+        required:true
+        }
+      },
+    fulfillment:{
+      speech:{
+        type: String,
+        required:true
+        }
+    }
+  },
+  sessionId:{
+        type: String,
+        required:true
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Tasks", TaskSchema);
