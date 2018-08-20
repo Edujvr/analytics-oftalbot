1a678995ebe1d8585c556321c62cee8ca4e87f40
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-08-20 14:35:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ require("./config/db");
 
 //Creación del metodo que escucha las llamadas POST y obtiene los parametros
 app.post("/webhook", (req, res) =>{  
-  var respuesta = req.body.result.fulfillment.speech;
- // console.log(req.body.originalRequest)	
+  console.log(req.body.originalRequest)	
   const action = req.body.result.action;
   const chatbase = require('@google/chatbase');
   const chatbase2= require('@google/chatbase');
-  
+  var respuesta = req.body.result.fulfillment.speech;
+	
 	//Consulta nombre de Generalista en Mongo Atlas 
 	if(action=='query'){
 		var query  = Colaboradores.where({ UsuarioRed: req.body.result.parameters.UsuariosRed });
@@ -29,90 +29,6 @@ app.post("/webhook", (req, res) =>{
 		    }
 			respuesta = colaboradores.Nombre +" Tu consultor es " + colaboradores.NombreConsultor //+" Tu nombre " +usuarioName
 			sendResponse(respuesta);
-			console.log("Atlas"+respuesta);
-			//Creción del Objeto Json para almacenar en Mongo Atlas
-				  var historial = new Object();
-				  historial.SesionId = req.body.sessionId;
-				  historial.UsuarioId = req.body.originalRequest.data.sender.id;
-				  historial.UsuarioDice = req.body.result.resolvedQuery;
-				  historial.NombreIntento= req.body.result.metadata.intentName;
-				  historial.BotResponde= respuesta;	
-				  //console.log(historial)
-				 console.log("Primer registro"+ respuesta);	
-
-			//Envio de objeto con mensaje a Mongo Atlas
-				let newHistorial = new Historial(historial);
-				  newHistorial.save((err, task) => {
-				    if (err) {
-				      res.status(500).send(err);
-				    }
-				    res.status(201).json(historial);
-				  });
-
-				// Creación mensaje Set de Usuario
-				var messageSet = chatbase.newMessageSet()
-				  .setApiKey("f8be6699-d8b4-44d8-90cb-07d8d2e98cf2") // Chatbase API key
-				  .setPlatform("Dialogflow") // Nombre de la Plataforma del Chat
-				  .setVersion('1.0'); // La versión que el bot desplegado es
-
-				// Mensaje del Usuario
-				if (action == "nothandled") {
-				messageSet.newMessage() // Crea una nueva instancia de Mensaje
-				  .setAsTypeUser() // Marca como mensaje que viene del Usuario
-				  .setUserId(req.body.sessionId) // ID de usuario en la plataforma de chat 
-				  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
-				  .setIntent(req.body.result.metadata.intentName) // La intención decodificada a partir del mensaje del usuario
-				  .setMessage(req.body.result.resolvedQuery) // Mensaje de Usuario
-				  .setAsNotHandled(); // Indica a Chatbase que marque esta solicitud de usuario como "no gestionada"(not handled)
-				} else {
-				  messageSet.newMessage() // Crea una nueva instancia de Mensaje
-				  .setAsTypeUser() // Marca como mensaje que viene del Usuario
-				  .setUserId(req.body.sessionId) // ID de usuario en la plataforma de chat 
-				  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
-				  .setIntent(req.body.result.metadata.intentName) // La intención decodificada a partir del mensaje del usuario
-				  .setMessage(req.body.result.resolvedQuery) // Mensaje de Usuario
-				  .setAsHandled(); // Marque esta solicitud como exitosamente manejada(handled)
-				}
-
-				// Envio de mensaje a Chatbase
-				messageSet.sendMessageSet()
-				  .then(messageSet => {
-				    console.log(messageSet.getCreateResponse());
-				  })
-				  .catch(error => {
-				    console.error(error);
-				  });	
-
-				// Creación mensaje Set del Bot
-				var messageSet2 = chatbase.newMessageSet()
-				  .setApiKey("f8be6699-d8b4-44d8-90cb-07d8d2e98cf2") // Chatbase API key
-				  .setPlatform("Dialogflow") // Nombre de la Plataforma del Chat
-				  .setVersion('1.0'); // La versión que el bot desplegado es
-
-				// Mensaje del Bot
-				const botMessage = messageSet2.newMessage() // Crea una nueva instancia de Mensaje
-				  .setAsTypeAgent() // Marca como mensaje que viene del Bot
-				  .setUserId(req.body.sessionId) // ID de usuario la misma que arriba
-				  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
-				  .setMessage(respuesta); // Mensaje de respuesta del Bot
-				console.log("Ultimo registro "+respuesta);
-				// Envio de mensaje a Chatbase
-				messageSet2.sendMessageSet()
-				  .then(messageSet => {
-				    console.log(messageSet2.getCreateResponse());
-				  })
-				  .catch(error => {
-				    console.error(error);
-				});
-
-
-			//Envio de información a Google Analytics libreria request
-				const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-123508749-1&cid='+req.body.sessionId+'&dh=www.google-analytics.com&ec=Intento&ea='+req.body.result.metadata.intentName+'&el='+req.body.result.resolvedQuery+'&ev=1&aip=1';
-					request.get(encodeURI(url))
-					.on('error', function(err){
-					if (err) throw err;
-					console.log('Successfully logged to GA , Response to Dialogflow');
-				});	
 		  });
 	 } else { //Envio de información directa webhook a Dialogflow		  
 	    res.json({
@@ -122,91 +38,93 @@ app.post("/webhook", (req, res) =>{
 		    contextOut: req.body.result.contexts,
 		    source: req.body.result.source
        		 });
-		 var historial = new Object();
-				  historial.SesionId = req.body.sessionId;
-				  historial.UsuarioId = req.body.originalRequest.data.sender.id;
-				  historial.UsuarioDice = req.body.result.resolvedQuery;
-				  historial.NombreIntento= req.body.result.metadata.intentName;
-				  historial.BotResponde= respuesta;	
-				  //console.log(historial)
-				 console.log("Primer registro"+ respuesta);	
-
-			//Envio de objeto con mensaje a Mongo Atlas
-				let newHistorial = new Historial(historial);
-				  newHistorial.save((err, task) => {
-				    if (err) {
-				      res.status(500).send(err);
-				    }
-				    res.status(201).json(historial);
-				  });
-
-				// Creación mensaje Set de Usuario
-				var messageSet = chatbase.newMessageSet()
-				  .setApiKey("f8be6699-d8b4-44d8-90cb-07d8d2e98cf2") // Chatbase API key
-				  .setPlatform("Dialogflow") // Nombre de la Plataforma del Chat
-				  .setVersion('1.0'); // La versión que el bot desplegado es
-
-				// Mensaje del Usuario
-				if (action == "nothandled") {
-				messageSet.newMessage() // Crea una nueva instancia de Mensaje
-				  .setAsTypeUser() // Marca como mensaje que viene del Usuario
-				  .setUserId(req.body.sessionId) // ID de usuario en la plataforma de chat 
-				  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
-				  .setIntent(req.body.result.metadata.intentName) // La intención decodificada a partir del mensaje del usuario
-				  .setMessage(req.body.result.resolvedQuery) // Mensaje de Usuario
-				  .setAsNotHandled(); // Indica a Chatbase que marque esta solicitud de usuario como "no gestionada"(not handled)
-				} else {
-				  messageSet.newMessage() // Crea una nueva instancia de Mensaje
-				  .setAsTypeUser() // Marca como mensaje que viene del Usuario
-				  .setUserId(req.body.sessionId) // ID de usuario en la plataforma de chat 
-				  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
-				  .setIntent(req.body.result.metadata.intentName) // La intención decodificada a partir del mensaje del usuario
-				  .setMessage(req.body.result.resolvedQuery) // Mensaje de Usuario
-				  .setAsHandled(); // Marque esta solicitud como exitosamente manejada(handled)
-				}
-
-				// Envio de mensaje a Chatbase
-				messageSet.sendMessageSet()
-				  .then(messageSet => {
-				    console.log(messageSet.getCreateResponse());
-				  })
-				  .catch(error => {
-				    console.error(error);
-				  });	
-
-				// Creación mensaje Set del Bot
-				var messageSet2 = chatbase.newMessageSet()
-				  .setApiKey("f8be6699-d8b4-44d8-90cb-07d8d2e98cf2") // Chatbase API key
-				  .setPlatform("Dialogflow") // Nombre de la Plataforma del Chat
-				  .setVersion('1.0'); // La versión que el bot desplegado es
-
-				// Mensaje del Bot
-				const botMessage = messageSet2.newMessage() // Crea una nueva instancia de Mensaje
-				  .setAsTypeAgent() // Marca como mensaje que viene del Bot
-				  .setUserId(req.body.sessionId) // ID de usuario la misma que arriba
-				  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
-				  .setMessage(respuesta); // Mensaje de respuesta del Bot
-				console.log("Ultimo registro "+respuesta);
-				// Envio de mensaje a Chatbase
-				messageSet2.sendMessageSet()
-				  .then(messageSet => {
-				    console.log(messageSet2.getCreateResponse());
-				  })
-				  .catch(error => {
-				    console.error(error);
-				});
-
-
-			//Envio de información a Google Analytics libreria request
-				const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-123508749-1&cid='+req.body.sessionId+'&dh=www.google-analytics.com&ec=Intento&ea='+req.body.result.metadata.intentName+'&el='+req.body.result.resolvedQuery+'&ev=1&aip=1';
-					request.get(encodeURI(url))
-					.on('error', function(err){
-					if (err) throw err;
-					console.log('Successfully logged to GA , Response to Dialogflow');
-				});	
 	 }
+		
 	
-    });
+//Creción del Objeto Json para almacenar en Mongo Atlas
+  var historial = new Object();
+  historial.SesionId = req.body.sessionId;
+  historial.UsuarioId = req.body.originalRequest.data.sender.id;
+  historial.UsuarioDice = req.body.result.resolvedQuery;
+  historial.NombreIntento= req.body.result.metadata.intentName;
+  historial.BotResponde= respuesta;	
+  console.log(historial)
+	
+	
+//Envio de objeto con mensaje a Mongo Atlas
+	let newHistorial = new Historial(historial);
+	  newHistorial.save((err, task) => {
+	    if (err) {
+	      res.status(500).send(err);
+	    }
+	    res.status(201).json(historial);
+	  });
+	
+	// Creación mensaje Set de Usuario
+	var messageSet = chatbase.newMessageSet()
+	  .setApiKey("f8be6699-d8b4-44d8-90cb-07d8d2e98cf2") // Chatbase API key
+	  .setPlatform("Dialogflow") // Nombre de la Plataforma del Chat
+	  .setVersion('1.0'); // La versión que el bot desplegado es
+
+	// Mensaje del Usuario
+	if (action == "nothandled") {
+	messageSet.newMessage() // Crea una nueva instancia de Mensaje
+	  .setAsTypeUser() // Marca como mensaje que viene del Usuario
+	  .setUserId(req.body.sessionId) // ID de usuario en la plataforma de chat 
+	  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
+	  .setIntent(req.body.result.metadata.intentName) // La intención decodificada a partir del mensaje del usuario
+	  .setMessage(req.body.result.resolvedQuery) // Mensaje de Usuario
+	  .setAsNotHandled(); // Indica a Chatbase que marque esta solicitud de usuario como "no gestionada"(not handled)
+	} else {
+	  messageSet.newMessage() // Crea una nueva instancia de Mensaje
+	  .setAsTypeUser() // Marca como mensaje que viene del Usuario
+	  .setUserId(req.body.sessionId) // ID de usuario en la plataforma de chat 
+	  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
+	  .setIntent(req.body.result.metadata.intentName) // La intención decodificada a partir del mensaje del usuario
+	  .setMessage(req.body.result.resolvedQuery) // Mensaje de Usuario
+	  .setAsHandled(); // Marque esta solicitud como exitosamente manejada(handled)
+	}
+
+	// Envio de mensaje a Chatbase
+	messageSet.sendMessageSet()
+	  .then(messageSet => {
+	    console.log(messageSet.getCreateResponse());
+	  })
+	  .catch(error => {
+	    console.error(error);
+	  });	
+	
+	// Creación mensaje Set del Bot
+	var messageSet2 = chatbase.newMessageSet()
+	  .setApiKey("f8be6699-d8b4-44d8-90cb-07d8d2e98cf2") // Chatbase API key
+	  .setPlatform("Dialogflow") // Nombre de la Plataforma del Chat
+	  .setVersion('1.0'); // La versión que el bot desplegado es
+	
+	// Mensaje del Bot
+	const botMessage = messageSet2.newMessage() // Crea una nueva instancia de Mensaje
+	  .setAsTypeAgent() // Marca como mensaje que viene del Bot
+	  .setUserId(req.body.sessionId) // ID de usuario la misma que arriba
+	  .setTimestamp(Date.now().toString()) // Tiempo obtenido del sistema
+	  .setMessage(respuesta); // Mensaje de respuesta del Bot
+	
+	// Envio de mensaje a Chatbase
+	messageSet2.sendMessageSet()
+	  .then(messageSet => {
+	    console.log(messageSet2.getCreateResponse());
+	  })
+	  .catch(error => {
+	    console.error(error);
+	});
+		
+//Envio de información a Google Analytics libreria request
+	const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-123508749-1&cid='+req.body.sessionId+'&dh=www.google-analytics.com&ec=Intento&ea='+req.body.result.metadata.intentName+'&el='+req.body.result.resolvedQuery+'&ev=1&aip=1';
+		request.get(encodeURI(url))
+       		.on('error', function(err){
+          	if (err) throw err;
+	  	console.log('Successfully logged to GA , Response to Dialogflow');
+        });	
+	
+	
 	//Envio de información webhook a Dialogflow Messenger
          function sendResponse (responseToUser) {
 	    // Si la respuesta es una cadena, envíela como respuesta al usuario
@@ -230,7 +148,4 @@ app.post("/webhook", (req, res) =>{
 	    }
 	  }
 	
-
-
-	
-
+    });
