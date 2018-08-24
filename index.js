e15058f3cd71a323a2896d2f877016d222578eea
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-08-24 10:18:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,12 +30,13 @@ app.post("/webhook",(req, res) =>{
   var respuesta = req.body.result.fulfillment.speech;
   //var idUsuario = req.body.originalRequest.data.sender.id;
   var idPrueba=	1718036691652143;
+	var graphObject
 	//console.log(idUsuario);
-	console.log(idPrueba);
+	//console.log(idPrueba);
 	var access_token = 'EAAC67570ZAXABAO31zYcwvFFwh3QUYX6ooZAZBeCgMEd2HUZCQ9NrD9TZAZB4MLlPEWONcQ9iFzZC3i4lhUAefK0E3mzB0oASiDylrqZAYBgEH1fdSggl0bjJqHObGUvv0YVVRSr7N7taxZCsNmYDvqXhnUQOtcBuY3EuTYJgMMwEv87uGWMWfZAZAT3hZAIAPRrQrXLr3naRSZB2TAZDZD';
-graph.setAccessToken(access_token);	
+	graph.setAccessToken(access_token);	
 	
-	var graphObject=graph.get("1718036691652143?fields=name,first_name,last_name", function(err, res){
+	graphObject = graph.get("1718036691652143?fields=name,first_name,last_name", function(err, res){
 		console.log(res);
 	});
 	
