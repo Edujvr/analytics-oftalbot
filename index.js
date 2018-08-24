7d54da940b5644a4c56f578bb3000ddc5c0aa138
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-08-24 10:06:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.post("/webhook",(req, res) =>{
   const chatbase2= require('@google/chatbase');
   var respuesta = req.body.result.fulfillment.speech;
   //var idUsuario = req.body.originalRequest.data.sender.id;
-  //var idPrueba=	1718036691652143;
-	console.log(idUsuario);
+  var idPrueba=	1718036691652143;
+	//console.log(idUsuario);
 	console.log(idPrueba);
 	var access_token = EAAC67570ZAXABAO31zYcwvFFwh3QUYX6ooZAZBeCgMEd2HUZCQ9NrD9TZAZB4MLlPEWONcQ9iFzZC3i4lhUAefK0E3mzB0oASiDylrqZAYBgEH1fdSggl0bjJqHObGUvv0YVVRSr7N7taxZCsNmYDvqXhnUQOtcBuY3EuTYJgMMwEv87uGWMWfZAZAT3hZAIAPRrQrXLr3naRSZB2TAZDZD;
 graph.setAccessToken(access_token);	
